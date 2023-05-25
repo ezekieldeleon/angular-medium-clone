3412f21fc1e5f7f458ff1f63c61af3c24a394b66
Ezekiel De Leon <ezekiel.de.leon@jci.com>
2023-05-25 09:38:26 -0700

    Set up again.

---
+++ src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import {Route} from '@angular/router'
+
+export const appRoutes: Route[] = [
+  {
+    path: 'register',
+    loadChildren: () =>
+      import('./auth/auth.routes').then((m) => m.registerRoutes),
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./auth/auth.routes').then((m) => m.loginRoutes),
+  },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./globalFeed/globalFeed.routes').then((m) => m.routes),
+  },
+]
