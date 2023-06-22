d6f5e384e2b4b5c8a3f360d9ce5a472002f7c885
Ezekiel De Leon <ezekiel.de.leon@jci.com>
2023-06-22 09:22:44 -0700

    Start settings page.

--- src/app/auth/store/effects.ts
+++ src/app/auth/store/effects.ts
@@ -115,3 +115,24 @@ export const redirectAfterLoginEffect = createEffect(
   },
   {functional: true, dispatch: false}
 )
+
+export const updateCurrentUserEffect = createEffect(
+  (actions$ = inject(Actions), authService = inject(AuthService)) => {
+    return actions$.pipe(
+      ofType(authActions.updateCurrentUser),
+      switchMap(({currentUserRequest}) => {
+        return authService.updateCurrentUser(currentUserRequest).pipe(
+          map((currentUser: CurrentUserInterface) => {
+            return authActions.updateCurrentUserSuccess({currentUser})
+          }),
+          catchError((errorResponse: HttpErrorResponse) => {
+            return of(
+              authActions.updateCurrentUserFailure(errorResponse.error.errors)
+            )
+          })
+        )
+      })
+    )
+  },
+  {functional: true}
+)
--- src/app/auth/store/reducers.ts
+++ src/app/auth/store/reducers.ts
@@ -58,6 +58,10 @@ const authFeature = createFeature({
       isLoading: false,
       currentUser: null,
     })),
+    on(authActions.updateCurrentUserSuccess, (state, action) => ({
+      ...state,
+      currentUser: action.currentUser,
+    })),
     on(routerNavigationAction, (state) => ({...state, validationErrors: null}))
   ),
 })
