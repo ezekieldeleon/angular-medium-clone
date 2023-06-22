4537d1ae6c631efcad029f233d0aef2dd85f6d9e
Ezekiel <ezekiel.de.leon@jci.com>
2023-06-22 10:53:03 -0700

    Continue settings page.

--- src/app/settings/components/settings.component.ts
+++ src/app/settings/components/settings.component.ts
@@ -1,8 +1,62 @@
-import {Component} from '@angular/core'
+import {CommonModule} from '@angular/common'
+import {Component, OnDestroy, OnInit} from '@angular/core'
+import {FormBuilder, ReactiveFormsModule} from '@angular/forms'
+import {Store, select} from '@ngrx/store'
+import {Subscription, combineLatest, filter} from 'rxjs'
+import {
+  selectCurrentUser,
+  selectIsSubmitting,
+  selectValidationErrors,
+} from 'src/app/auth/store/reducers'
+import {BackendErrorMessages} from 'src/app/shared/components/backendErrorMessages/backendErrorMessages.component'
+import {CurrentUserInterface} from 'src/app/shared/types/currentUser.interface'
 
 @Component({
   selector: 'mc-settings',
   templateUrl: './settings.component.html',
   standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, BackendErrorMessages],
 })
-export class SettingsComponent {}
+export class SettingsComponent implements OnInit, OnDestroy {
+  form = this.fb.nonNullable.group({
+    image: this.fb.nonNullable.control(''),
+    username: this.fb.nonNullable.control(''),
+    bio: this.fb.nonNullable.control(''),
+    email: this.fb.nonNullable.control(''),
+    password: this.fb.nonNullable.control(''),
+  })
+  currentUser?: CurrentUserInterface
+  data$ = combineLatest({
+    isSubmitting: this.store.select(selectIsSubmitting),
+    backendErrors: this.store.select(selectValidationErrors),
+  })
+  currentUserSubscription?: Subscription
+  constructor(private fb: FormBuilder, private store: Store) {}
+  ngOnDestroy(): void {
+    this.currentUserSubscription?.unsubscribe()
+  }
+
+  ngOnInit(): void {
+    this.currentUserSubscription = this.store
+      .pipe(select(selectCurrentUser), filter(Boolean))
+      .subscribe((currentUser) => {
+        this.currentUser = currentUser
+        this.initializeForm()
+      })
+  }
+
+  initializeForm(): void {
+    if (!this.currentUser) {
+      throw new Error('currentUser is undefined')
+    }
+    this.form.patchValue({
+      image: this.currentUser.image ?? '',
+      username: this.currentUser.username,
+      bio: this.currentUser.bio ?? '',
+      email: this.currentUser.email,
+      password: '',
+    })
+  }
+
+  submit(): void {}
+}
